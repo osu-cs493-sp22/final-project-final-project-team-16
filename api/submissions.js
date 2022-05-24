8e36c3938e1ec66434e93d4151a9d03c7c2bfaa7
Timothy Nguyen <timothynguyen2000@yahoo.com>
2022-05-24 15:12:49 -0700

    Moved Submission schema to models

--- api/submissions.js
+++ api/submissions.js
@@ -1,14 +1,1 @@
 
-const { ObjectId } = require('mongodb')
-const { getDbInstance } = require('../lib/mongo')
-const { extractValidFields } = require('../lib/validation')
-
-const SubmissionsSchema = {
-    assugnmentId: {required: true},
-    studentId: {required: true},
-    timestamp: {required: true},
-    grade: {required: true},
-    submission: {required: true}
-}
-
-exports.SubmissionsSchema = SubmissionsSchema
