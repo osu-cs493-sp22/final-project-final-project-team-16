68562325096c08bd01622fcc3419dc24d548ee60
Timothy Nguyen <timothynguyen2000@yahoo.com>
2022-05-31 16:35:52 -0700

    Created POST /submissions endpoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 const router = module.exports = require('express').Router();
 
 router.use('/assignments', require('./assignments').router);
+router.use('/submissions', require('./submissions').router);
