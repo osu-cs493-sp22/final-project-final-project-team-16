d161c4d966f8115ec8f1475244ed8d625913f0ee
Juichilee <leejuic@oregonstate.edu>
2022-06-08 20:15:35 -0700

    Added models/user reference to models/course

--- models/course.js
+++ models/course.js
@@ -2,6 +2,7 @@ const {ObjectId} = require ('mongodb')
 
 const { getDbReference } = require('../lib/mongo')
 const { extractValidFields } = require('../lib/validation')
+const { UserSchema,addEnrolled } = require('../models/user')
 
 const courseSchema = {
     subject: { required: true },
